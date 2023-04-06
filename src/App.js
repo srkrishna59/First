446d60dc5d9909e4c6cf5fe0f490b2d6f540852e
krishna <srkrishna@abcgenesis.com>
2023-04-06 13:56:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React First
+          Learn React First branch
         </a>
       </header>
     </div>
