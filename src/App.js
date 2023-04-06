07a9235fdb7da0741ed4bd8e3f0a58621cfe39fe
krishna <srkrishna@abcgenesis.com>
2023-04-06 15:35:22 +0530

    Second Developer First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React First branch 
+          Learn React Second branch 
         </a>
       </header>
     </div>
